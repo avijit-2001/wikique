868b6e13011aa52cb21532e1afebc5bba602b531
Ajay <ajay@Ajays-MacBook-Pro.local>
2023-04-17 18:36:50 +0530

    minor changes img size

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,26 +3,27 @@ import "../../node_modules/bootstrap/dist/css/bootstrap.min.css";
 const Card = ({ name, logoURL, duration }) => {
   return (
     <div className="row">
-      <div className="col-md-6">
-        <div
-          class="card"
-          style={{ height: "15.8rem", width: "11.8rem", margin: 7.5, marginTop: 15 }}
-        >
-          <img src={logoURL} className="card-img-top" alt="..."  width="400" height="500" />
-          <div className="card-body">
-            <h6 className="card-title">{name}</h6>
-            <p class="card-text">
-              <b>Duration: </b> {duration} mins
-            </p>
-          </div>
-          <div class="card-footer">
-          <a href="/" class="btn btn-primary">
-              Overview
-            </a>
-          </div>
-          </div>
-        </div>        
+  <div className="col-md-6">
+    <div
+      className="card"
+      style={{ height: "15rem", width: "12.5rem", margin: 20, marginTop: 15, marginLeft: 10}}
+    >
+      <img src={logoURL} className="card-img-top" alt="..." height="80"  />
+      <div className="card-body">
+        <h6 className="card-title">{name}</h6>
+        <p className="card-text">
+          <b>Duration:</b> {duration} mins
+        </p>
+      </div>
+      <div className="card-footer">
+        <a href="/" className="btn btn-primary">
+          Overview
+        </a>
+      </div>
     </div>
+  </div>        
+</div>
+
   );
 };
 
