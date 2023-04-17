e70219a6750865fa19d10b48dd869426d1126508
Ajay <ajay@Ajays-MacBook-Pro.local>
2023-04-17 16:17:32 +0530

    changes for uniform card size

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,20 +5,23 @@ const Card = ({ name, logoURL, duration }) => {
     <div className="row">
       <div className="col-md-6">
         <div
-          className="card"
-          style={{ width: "13.8rem", margin: 2.5, marginTop: 5 }}
+          class="card"
+          style={{ height: "15.8rem", width: "11.8rem", margin: 2.5, marginTop: 5 }}
         >
           <img src={logoURL} className="card-img-top" alt="..." />
           <div className="card-body">
-            <h5 className="card-title">{name}</h5>
+            <h6 className="card-title">{name}</h6>
             <p class="card-text">
               <b>Duration: </b> {duration} mins
             </p>
-            <a href="/" class="btn btn-primary">
+          </div>
+          <div class="card-footer">
+          <a href="/" class="btn btn-primary">
               Overview
             </a>
           </div>
         </div>
+
       </div>
     </div>
   );
