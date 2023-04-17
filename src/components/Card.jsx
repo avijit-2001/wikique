7faca81cbe649ee3cf9197506382b87824395488
Ajay <ajay@Ajays-MacBook-Pro.local>
2023-04-17 16:38:03 +0530

    card body allign

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,9 +6,9 @@ const Card = ({ name, logoURL, duration }) => {
       <div className="col-md-6">
         <div
           class="card"
-          style={{ height: "15.8rem", width: "11.8rem", margin: 2.5, marginTop: 5 }}
+          style={{ height: "15.8rem", width: "11.8rem", margin: 7.5, marginTop: 15 }}
         >
-          <img src={logoURL} className="card-img-top" alt="..." />
+          <img src={logoURL} className="card-img-top" alt="..."  width="400" height="500" />
           <div className="card-body">
             <h6 className="card-title">{name}</h6>
             <p class="card-text">
@@ -20,9 +20,8 @@ const Card = ({ name, logoURL, duration }) => {
               Overview
             </a>
           </div>
-        </div>
-
-      </div>
+          </div>
+        </div>        
     </div>
   );
 };
